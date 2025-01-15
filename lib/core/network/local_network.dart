918b9f093ec890a811150c9fb6ad486b967bf407
Acer <karanbohara216@gmail.com>
2025-01-15 21:42:42 +0545

    theme added

---
+++ lib/core/network/local_network.dart
@@ -0,0 +1,9 @@
+class LocalNetwork {
+  // User Queries
+  Future<void> registerUser(
+      String username, String email, String password) async {}
+  Future<void> deleteUser() async {}
+  Future<void> getAllUsers() async {}
+  Future<void> updateUser() async {}
+  Future<void> loginUser(String username, String password) async {}
+}
