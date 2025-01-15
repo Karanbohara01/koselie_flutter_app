99f6bbbf9a66ad3f81adc5f84dfa5af4544744bd
Acer <karanbohara216@gmail.com>
2025-01-15 21:24:38 +0545

    generic_usecase created

---
+++ lib/app/usecase/usecase.dart
@@ -0,0 +1,10 @@
+import 'package:dartz/dartz.dart';
+import 'package:koselie/core/error/failure.dart';
+
+abstract interface class UsecaseWithParams<SuccessType, Params> {
+  Future<Either<Failure, SuccessType>> call(Params params);
+}
+
+abstract interface class UsecaseWithoutParams<SuccessType> {
+  Future<Either<Failure, SuccessType>> call();
+}
