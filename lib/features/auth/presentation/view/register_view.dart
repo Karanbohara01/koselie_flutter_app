ee2f29133c2ea67ea7e1d6e63b35db291057cd6c
Acer <karanbohara216@gmail.com>
2025-01-17 13:32:54 +0545

    register page modified

--- lib/features/auth/presentation/view/register_view.dart
+++ lib/features/auth/presentation/view/register_view.dart
@@ -1,101 +1,127 @@
 // import 'package:flutter/material.dart';
 // import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:koselie/features/auth/presentation/view/login_view.dart';
 // import 'package:koselie/features/auth/presentation/view_model/signup/register_bloc.dart';
 
-// class RegisterView extends StatefulWidget {
+// class RegisterView extends StatelessWidget {
 //   const RegisterView({super.key});
 
 //   @override
-//   State<RegisterView> createState() => _RegisterViewState();
-// }
-
-// class _RegisterViewState extends State<RegisterView> {
-//   final _gap = const SizedBox(height: 8);
-//   final _key = GlobalKey<FormState>();
-//   final _userNameController = TextEditingController(text: '');
-//   final _emailController = TextEditingController(text: '');
-//   final _passwordController = TextEditingController(text: '');
-
-//   @override
 //   Widget build(BuildContext context) {
+//     final userNameController = TextEditingController();
+//     final emailController = TextEditingController();
+//     final passwordController = TextEditingController();
+//     final formKey = GlobalKey<FormState>();
+
 //     return Scaffold(
 //       appBar: AppBar(
-//         title: BlocBuilder<RegisterBloc, RegisterState>(
-//           builder: (context, state) {
-//             return const Text('Register User');
-//           },
-//         ),
+//         title: const Text('Register User'),
 //         centerTitle: true,
+//         backgroundColor: Colors.pink, // Adding a color for the AppBar
 //       ),
 //       body: SafeArea(
-//         child: SingleChildScrollView(
-//           child: Padding(
-//             padding: const EdgeInsets.all(8),
-//             child: Form(
-//               key: _key,
-//               child: Column(
-//                 children: [
-//                   const SizedBox(height: 25),
-//                   TextFormField(
-//                     controller: _userNameController,
-//                     decoration: const InputDecoration(
-//                       labelText: 'User Name',
+//         child: Padding(
+//           padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
+//           child: Form(
+//             key: formKey,
+//             child: Column(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               crossAxisAlignment: CrossAxisAlignment.start,
+//               children: [
+//                 Text(
+//                   'Create an Account',
+//                   style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+//                         fontWeight: FontWeight.bold,
+//                         color: Colors.pink,
+//                       ),
+//                 ),
+//                 const SizedBox(height: 20),
+//                 TextFormField(
+//                   controller: userNameController,
+//                   decoration: InputDecoration(
+//                     labelText: 'User Name',
+//                     labelStyle: const TextStyle(fontSize: 16),
+//                     border: OutlineInputBorder(
+//                       borderRadius: BorderRadius.circular(10),
+//                       borderSide: const BorderSide(color: Colors.pink),
 //                     ),
-//                     validator: ((value) {
-//                       if (value == null || value.isEmpty) {
-//                         return 'Please enter first name';
+//                     contentPadding: const EdgeInsets.symmetric(
+//                         horizontal: 20, vertical: 18),
+//                   ),
+//                   validator: (value) => value?.isEmpty ?? true
+//                       ? 'Please enter a user name'
+//                       : null,
+//                 ),
+//                 const SizedBox(height: 16),
+//                 TextFormField(
+//                   controller: passwordController,
+//                   decoration: InputDecoration(
+//                     labelText: 'Password',
+//                     labelStyle: const TextStyle(fontSize: 16),
+//                     border: OutlineInputBorder(
+//                       borderRadius: BorderRadius.circular(10),
+//                       borderSide: const BorderSide(
+//                         color: Colors.pink,
+//                       ),
+//                     ),
+//                     contentPadding: const EdgeInsets.symmetric(
+//                         horizontal: 20, vertical: 18),
+//                   ),
+//                   obscureText: true,
+//                   validator: (value) =>
+//                       value?.isEmpty ?? true ? 'Please enter a password' : null,
+//                 ),
+//                 const SizedBox(height: 16),
+//                 TextFormField(
+//                   controller: emailController,
+//                   decoration: InputDecoration(
+//                     labelText: 'Email',
+//                     labelStyle: const TextStyle(fontSize: 16),
+//                     border: OutlineInputBorder(
+//                       borderRadius: BorderRadius.circular(10),
+//                       borderSide: const BorderSide(color: Colors.pink),
+//                     ),
+//                     contentPadding: const EdgeInsets.symmetric(
+//                         horizontal: 20, vertical: 18),
+//                   ),
+//                   validator: (value) =>
+//                       value?.isEmpty ?? true ? 'Please enter an email' : null,
+//                 ),
+//                 const SizedBox(height: 24),
+//                 SizedBox(
+//                   width: double.infinity,
+//                   child: ElevatedButton(
+//                     onPressed: () {
+//                       if (formKey.currentState?.validate() ?? false) {
+//                         context.read<RegisterBloc>().add(
+//                               RegisterUser(
+//                                 context: context,
+//                                 userName: userNameController.text,
+//                                 email: emailController.text,
+//                                 password: passwordController.text,
+//                               ),
+//                             );
+//                         // After successful registration, navigate to login screen
+//                         Navigator.pushReplacement(
+//                           context,
+//                           MaterialPageRoute(builder: (context) => LoginView()),
+//                         );
 //                       }
-//                       return null;
-//                     }),
-//                   ),
-//                   const SizedBox(height: 25),
-//                   TextFormField(
-//                     controller: _passwordController,
-//                     decoration: const InputDecoration(
-//                       labelText: 'Password',
+//                     },
+//                     style: ElevatedButton.styleFrom(
+//                       backgroundColor: Colors.pink, // Button background color
+//                       padding: const EdgeInsets.symmetric(vertical: 16),
+//                       shape: RoundedRectangleBorder(
+//                         borderRadius: BorderRadius.circular(10),
+//                       ),
 //                     ),
-//                     validator: ((value) {
-//                       if (value == null || value.isEmpty) {
-//                         return 'Please enter password.';
-//                       }
-//                       return null;
-//                     }),
-//                   ),
-//                   _gap,
-//                   TextFormField(
-//                     controller: _emailController,
-//                     obscureText: true,
-//                     decoration: const InputDecoration(
-//                       labelText: 'Email',
-//                     ),
-//                     validator: ((value) {
-//                       if (value == null || value.isEmpty) {
-//                         return 'Please enter email';
-//                       }
-//                       return null;
-//                     }),
-//                   ),
-//                   _gap,
-//                   SizedBox(
-//                     width: double.infinity,
-//                     child: ElevatedButton(
-//                       onPressed: () {
-//                         if (_key.currentState!.validate()) {
-//                           context.read<RegisterBloc>().add(
-//                                 RegisterUser(
-//                                   context: context,
-//                                   userName: _userNameController.text,
-//                                   email: _emailController.text,
-//                                   password: _passwordController.text,
-//                                 ),
-//                               );
-//                         }
-//                       },
-//                       child: const Text('Register'),
+//                     child: const Text(
+//                       'Register',
+//                       style: TextStyle(fontSize: 16),
 //                     ),
 //                   ),
-//                 ],
-//               ),
+//                 ),
+//               ],
 //             ),
 //           ),
 //         ),
@@ -121,21 +147,23 @@ class RegisterView extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Register User'),
+        title: const Text(
+          'Register',
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
         centerTitle: true,
-        backgroundColor: Colors.pink, // Adding a color for the AppBar
+        backgroundColor: Colors.pink,
       ),
       body: SafeArea(
-        child: Padding(
+        child: SingleChildScrollView(
           padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
           child: Form(
             key: formKey,
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Text(
-                  'Create an Account',
+                  'Create Your Account',
                   style: Theme.of(context).textTheme.headlineSmall?.copyWith(
                         fontWeight: FontWeight.bold,
                         color: Colors.pink,
@@ -146,6 +174,7 @@ class RegisterView extends StatelessWidget {
                   controller: userNameController,
                   decoration: InputDecoration(
                     labelText: 'User Name',
+                    prefixIcon: const Icon(Icons.person, color: Colors.pink),
                     labelStyle: const TextStyle(fontSize: 16),
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(10),
@@ -160,9 +189,35 @@ class RegisterView extends StatelessWidget {
                 ),
                 const SizedBox(height: 16),
                 TextFormField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    prefixIcon: const Icon(Icons.email, color: Colors.pink),
+                    labelStyle: const TextStyle(fontSize: 16),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(color: Colors.pink),
+                    ),
+                    contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 18),
+                  ),
+                  validator: (value) {
+                    if (value?.isEmpty ?? true) {
+                      return 'Please enter an email';
+                    }
+                    final emailRegex = RegExp(
+                        r'^[a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z]+'); // Basic email validation
+                    return emailRegex.hasMatch(value!)
+                        ? null
+                        : 'Enter a valid email';
+                  },
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
                   controller: passwordController,
                   decoration: InputDecoration(
                     labelText: 'Password',
+                    prefixIcon: const Icon(Icons.lock, color: Colors.pink),
                     labelStyle: const TextStyle(fontSize: 16),
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(10),
@@ -174,24 +229,15 @@ class RegisterView extends StatelessWidget {
                         horizontal: 20, vertical: 18),
                   ),
                   obscureText: true,
-                  validator: (value) =>
-                      value?.isEmpty ?? true ? 'Please enter a password' : null,
-                ),
-                const SizedBox(height: 16),
-                TextFormField(
-                  controller: emailController,
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    labelStyle: const TextStyle(fontSize: 16),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: const BorderSide(color: Colors.pink),
-                    ),
-                    contentPadding: const EdgeInsets.symmetric(
-                        horizontal: 20, vertical: 18),
-                  ),
-                  validator: (value) =>
-                      value?.isEmpty ?? true ? 'Please enter an email' : null,
+                  validator: (value) {
+                    if (value?.isEmpty ?? true) {
+                      return 'Please enter a password';
+                    }
+                    if (value!.length < 6) {
+                      return 'Password must be at least 6 characters';
+                    }
+                    return null;
+                  },
                 ),
                 const SizedBox(height: 24),
                 SizedBox(
@@ -207,15 +253,16 @@ class RegisterView extends StatelessWidget {
                                 password: passwordController.text,
                               ),
                             );
-                        // After successful registration, navigate to login screen
                         Navigator.pushReplacement(
                           context,
-                          MaterialPageRoute(builder: (context) => LoginView()),
+                          MaterialPageRoute(
+                            builder: (context) => LoginView(),
+                          ),
                         );
                       }
                     },
                     style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.pink, // Button background color
+                      backgroundColor: Colors.pink,
                       padding: const EdgeInsets.symmetric(vertical: 16),
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(10),
@@ -223,9 +270,35 @@ class RegisterView extends StatelessWidget {
                     ),
                     child: const Text(
                       'Register',
-                      style: TextStyle(fontSize: 16),
+                      style: TextStyle(fontSize: 18, color: Colors.white),
                     ),
                   ),
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Already have an account? ',
+                      style: TextStyle(fontSize: 14),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(builder: (context) => LoginView()),
+                        );
+                      },
+                      child: const Text(
+                        'Login',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.pink,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
